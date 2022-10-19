e14651c66fb1de90b3b27fa8faf14744d95cafa0
capmahin <mahinulhuque1996@gmail.com>
2022-10-19 11:58:42 +0600

    more ditals

---
+++ src/Pages/Page.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+const Page = () => {
+  const [state, setState] = useState({
+    showMore: false,
+  });
+  const sampleData = [
+    {
+      name: "Shadman",
+      profession: "JS Developer",
+      age: 28,
+    },
+    {
+      name: "Mahin",
+      profession: "TS Developer",
+      age: 26,
+    },
+    {
+      name: "Mahin",
+      profession: "TS Developer",
+      age: 26,
+    },
+    {
+      name: "Mahin",
+      profession: "TS Developer",
+      age: 26,
+    },
+    {
+      name: "Mahin",
+      profession: "TS Developer",
+      age: 26,
+    },
+    {
+      name: "Mahin",
+      profession: "TS Developer",
+      age: 26,
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-2 max-w-screen-lg gap-4">
+      {!state.showMore && (
+        <div className="col-span-2 text-center" id="read-more-button">
+          <button
+            className="px-2 py-1 rounded-md border-2 border-red-600"
+            onClick={() =>
+              setState((prev) => ({
+                ...prev,
+                showMore: !prev.showMore,
+              }))
+            }
+          >
+            Read More
+          </button>
+        </div>
+      )}
+      {state.showMore &&
+        sampleData.map((each) => {
+          const { name, profession, age } = each;
+          return <Card key={name} {...{ name, age, profession }} />;
+        })}
+      {state.showMore && (
+        <div className="col-span-2 text-center" id="read-less-button">
+          <button
+            className="px-2 py-1 rounded-md border-2 border-red-600"
+            onClick={() =>
+              setState((prev) => ({
+                ...prev,
+                showMore: !prev.showMore,
+              }))
+            }
+          >
+            Read Less
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Card = (props) => {
+  const { name, age, profession } = props;
+  return (
+    <div className="container px-10 py-6 rounded-lg border-2 border-black m-4 col-span-1">
+      <p>{name}</p>
+      <p>{age}</p>
+      <p>{profession}</p>
+    </div>
+  );
+};
+
+export default Page;
