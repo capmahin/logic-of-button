a2a88dee83a9f7a4a8c43cd81b35d93d9693a150
capmahin <mahinulhuque1996@gmail.com>
2022-10-19 12:10:58 +0600

    add simple commnets

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 const Page = () => {
+  // Declaring the state here
+
   const [state, setState] = useState({
     showMore: false,
   });
@@ -39,6 +41,7 @@ const Page = () => {
 
   return (
     <div className="grid grid-cols-2 max-w-screen-lg gap-4">
+      {/* Express the logic false by using spray oparetor */}
       {!state.showMore && (
         <div className="col-span-2 text-center" id="read-more-button">
           <button
@@ -49,6 +52,7 @@ const Page = () => {
                 showMore: !prev.showMore,
               }))
             }
+            // Declaring the setState here
           >
             Read More
           </button>
@@ -59,15 +63,19 @@ const Page = () => {
           const { name, profession, age } = each;
           return <Card key={name} {...{ name, age, profession }} />;
         })}
+      {/* Add logic true  */}
       {state.showMore && (
         <div className="col-span-2 text-center" id="read-less-button">
           <button
             className="px-2 py-1 rounded-md border-2 border-red-600"
-            onClick={() =>
-              setState((prev) => ({
-                ...prev,
-                showMore: !prev.showMore,
-              }))
+            // add onClick and declaring the setState
+            onClick={
+              () =>
+                setState((prev) => ({
+                  ...prev,
+                  showMore: !prev.showMore,
+                }))
+              // finish the simple logic
             }
           >
             Read Less
